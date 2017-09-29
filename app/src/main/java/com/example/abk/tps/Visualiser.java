e7819312be9344b4b3238fefd55aaa06d716675d
marouaneaba <mar.abakarim@gmail.com>
2017-09-29 02:26:56 +0200

    ajout class Visualiser , visualiser les contact dans une listView

---
+++ app/src/main/java/com/example/abk/tps/Visualiser.java
@@ -0,0 +1,19 @@
+package com.example.abk.tps;
+
+/**
+ * Created by abk on 29/09/2017.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class Visualiser extends AppCompatActivity {
+
+
+
+}
